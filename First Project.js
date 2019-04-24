a4be82af013a096ede80573d21c576c7fe2eb4a4
Alamatos <alamatos@gmail.com>
2019-04-24 19:52:44 +0100

    First exercise of JS

--- First Project.js
+++ First Project.js
@@ -1,9 +1,18 @@
-const as= "bom dia"
-console.log(as)
+// Problem:
 
-var bss= "Boa tarde..."
+// Change the values of the variables A an B
 
-console.log(bs)
-var sbs= "Boa noite..."
+var A = 10
+var B = 20
+var C = null
 
-console.log(sb)
+// in order to change the values between variables we need to do as follows:
+
+C=A
+A=B
+B=C
+C=null
+
+console.log('A should be 20, and after the changes is: ' + A)
+console.log('B should be 10, and after the changes is: ' + B)
+console.log('C should continue to be null, and is : '+ C)
