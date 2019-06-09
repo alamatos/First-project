b16bb20741853662146a18844c189e2d4683d913
Alamatos <alamatos@gmail.com>
2019-06-09 11:23:38 +0100

    some chages in arrays commit

--- JavaScript/arrays.js
+++ JavaScript/arrays.js
@@ -1,26 +1,26 @@
-var fruitList= Array()
+var fruitList = Array()
 
-fruitList [0] = 'Banana'
-fruitList [1] = 'Orange'
-fruitList [2] = 'Strawberry'
-fruitList [3] = 'apple'
+fruitList[0] = 'Banana'
+fruitList[1] = 'Orange'
+fruitList[2] = 'Strawberry'
+fruitList[3] = 'apple'
 
 console.log(fruitList[2])
 
 var sports = []
-sports [0] = 'soccer'
-sports [1] = 'golf'
-sports [2] = 'F1'
-sports [3] = 'Basketball'
+sports[0] = 'soccer'
+sports[1] = 'golf'
+sports[2] = 'F1'
+sports[3] = 'Basketball'
 
-console.log( sports [3])
+console.log(sports[3])
 
 var laptops = Array('HP', 'ASUS', 'TOSHIBA', 'ACER')
 console.log(laptops[1])
 
 var cities = ['Lisbon', 'London', 'Mozambique', 'Bucharest']
 
-console.log(cities [0])
+console.log(cities[0])
 
 console.log(fruitList)
 console.log(sports)
@@ -29,20 +29,20 @@ console.log(cities)
 
 
 
-var food =[]
-food['fruits']=[]
+var food = []
+food['fruits'] = []
 
-food ['fruits'][0] = 'apple'
-food ['fruits'][1] ='Banana'
-food ['fruits'][2] ='Orange'
-food ['fruits'][3] = 'Strawberry'
+food['fruits'][0] = 'apple'
+food['fruits'][1] = 'Banana'
+food['fruits'][2] = 'Orange'
+food['fruits'][3] = 'Strawberry'
 
 food['icecream'] = []
 
-food ['icecream'][0] = 'Orange'
-food ['icecream'][1] = 'apple'
-food ['icecream'][2] = 'Strawberry'
-food ['icecream'][3] = 'Banana'
+food['icecream'][0] = 'Orange'
+food['icecream'][1] = 'apple'
+food['icecream'][2] = 'Strawberry'
+food['icecream'][3] = 'Banana'
 
 console.log(food)
 console.log(food['fruits'][0])
@@ -63,11 +63,13 @@ console.log(food['fruits'])
 
 // add an element to the beginning of the array
 
-food ['icecream'].unshift('Stracciatella')
+food['icecream'].unshift('Stracciatella')
 console.log(food['icecream'])
 
 // When is relevant to know if an element is inside of an array and what position is this element
 console.log(food['fruits'].indexOf('Banana'))
+// When is relevant to know if an element is inside of an array
+console.log(food['fruits'].includes('Banana'))
 
 // To sort an array, we need to use the method sort()
 // this type of method only works to alphanumeric sort
@@ -77,17 +79,17 @@ console.log(food['fruits'].sort())
 // to sort numbers we need to add a function, as i demonstrat below
 
 var euromillions = []
-euromillions [0] = 10
-euromillions [1] = 45
-euromillions [2] = 25
-euromillions [3] = 95
-euromillions [4] = 4
-euromillions [5] = 25
-euromillions [6] = 19
-euromillions [7] = 38
-euromillions [8] = 54
-euromillions [9] = 67
-euromillions [10] = 45
+euromillions[0] = 10
+euromillions[1] = 45
+euromillions[2] = 25
+euromillions[3] = 95
+euromillions[4] = 4
+euromillions[5] = 25
+euromillions[6] = 19
+euromillions[7] = 38
+euromillions[8] = 54
+euromillions[9] = 67
+euromillions[10] = 45
 
 console.log(euromillions.sort(sortValues))
 
@@ -96,5 +98,5 @@ console.log(euromillions.sort(sortValues))
 //  if (num1-num2) =0 num2 will maintain the initial position
 
 function sortValues(num1, num2) {
-    return num1-num2
+    return num1 - num2
 }
