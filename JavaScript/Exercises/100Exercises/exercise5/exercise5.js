3c8ffc7cadd847630e1b8fb5fe5a38153b9dc160
Alamatos <alamatos@gmail.com>
2019-06-16 17:47:18 +0100

    more developments in exercise5 commit

--- JavaScript/Exercises/100Exercises/exercise5/exercise5.js
+++ JavaScript/Exercises/100Exercises/exercise5/exercise5.js
@@ -1,4 +1,4 @@
-const functions = {
+const wordAnalysis = {
     type: (word) => typeof (word),
     whiteSpaces: (word) => {
         let wordAnalysis = word.includes(' ')
@@ -8,12 +8,22 @@ const functions = {
         } else {
             return "It doesn't has spaces"
         }
+    },
+    numberAnalysis: (word) => {
+        let isANumber = typeof (word) === 'number' ? true : false
+        return isANumber
+    },
+    isAlphanumeric : (word) => {
+        let patt = /[a-z]/g && /[0-9]/g
+        let Alphanumeric = word.match(patt)
+        //  ? 'alphanumeric' : 'non alphanumeric'
+        return Alphanumeric
     }
 }
+console.log(wordAnalysis.isAlphanumeric('1254as'))
 
 
-
-function isANumber(word) {
+function numberAnalysis(word) {
     let isANumber = typeof (word) === 'number' ? true : false
     return isANumber
 }
@@ -22,9 +32,8 @@ function isANumber(word) {
 // în order to avoid overwriting the module.exports, it is needed to do one of the following:
 
 // 1)
-module.exports.a = functions
-module.exports.b = isANumber
-
+module.exports.firstPlace = wordAnalysis
+module.exports.secondPlace = numberAnalysis
 // 2)
 
 // module.exports = {
--- JavaScript/Exercises/100Exercises/exercise5/exercise5.test.js
+++ JavaScript/Exercises/100Exercises/exercise5/exercise5.test.js
@@ -1,29 +1,34 @@
-const functions = require('./exercise5')
-const isANumber = require('./exercise5')
-
-
+const wordAnalysis = require('./exercise5')
+const numberAnalysis = require('./exercise5')
 
 
 test('It should give me the type of what was typed', () => {
-    expect(functions.a.type('teste')).toEqual('string')
+    expect(wordAnalysis.firstPlace.type('teste')).toEqual('string')
 })
 
 test('It should give me the type of what was typed', () => {
-    expect(functions.a.type(22)).toEqual('number')
+    expect(wordAnalysis.firstPlace.type(22)).toEqual('number')
 })
 
 
 test("It should give true if waht is typed has at least a space or false if it doesn't", () => {
-    expect(functions.a.whiteSpaces('Jon is cool')).toEqual('It has spaces');
+    expect(wordAnalysis.firstPlace.whiteSpaces('Jon is cool')).toEqual('It has spaces');
 })
 test("It should give true if what is typed has at least a space or false if it doesn't", () => {
-    expect(functions.a.whiteSpaces('AllTogetherNow')).toBe("It doesn't has spaces");
+    expect(wordAnalysis.firstPlace.whiteSpaces('AllTogetherNow')).toBe("It doesn't has spaces");
 })
 
-test('it should analyse if the given expression is a number or not', () => {
-    expect(isANumber.b(2)).toBeTruthy()
+test('it should analyse if the given expression is a number or not - Object', () => {
+    expect(wordAnalysis.firstPlace.numberAnalysis(2)).toBeTruthy();
 })
 
-test('it should analyse if the given expression is a number or not', () => {
-    expect(isANumber.b('jon')).toBeFalsy()
+test('it should analyse if the given expression is a number or not - Object', () => {
+    expect(wordAnalysis.firstPlace.numberAnalysis('jon')).toBeFalsy();
+})
+test('it should analyse if the given expression is a number or not - function', () => {
+    expect(numberAnalysis.secondPlace(2)).toBeTruthy();
+})
+
+test('it should analyse if the given expression is a number or not - function', () => {
+    expect(numberAnalysis.secondPlace('jon')).toBeFalsy();
 })
