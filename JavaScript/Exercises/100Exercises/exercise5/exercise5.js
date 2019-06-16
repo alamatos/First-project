69a1c2a56c24810a80e93792b5157521f93490fa
Alamatos <alamatos@gmail.com>
2019-06-16 11:36:25 +0100

    more testing in exercise 5 commit

--- JavaScript/Exercises/100Exercises/exercise5/exercise5.js
+++ JavaScript/Exercises/100Exercises/exercise5/exercise5.js
@@ -12,11 +12,33 @@ const functions = {
 }
 
 
+
 function isANumber(word) {
     let isANumber = typeof (word) === 'number' ? true : false
     return isANumber
 }
 
 
-module.exports = functions
-module.exports = isANumber
+// în order to avoid overwriting the module.exports, it is needed to do one of the following:
+
+// 1)
+module.exports.a = functions
+module.exports.b = isANumber
+
+// 2)
+
+// module.exports = {
+    // functions: functions,
+    // isANumber: isANumber
+// }
+
+// 3)
+// exports.functions = functions
+// exports.isANumber = isANumber
+
+// 4)
+
+// exports = {
+//     functions: functions,
+//     isANumber: isANumber
+// }
--- JavaScript/Exercises/100Exercises/exercise5/exercise5.test.js
+++ JavaScript/Exercises/100Exercises/exercise5/exercise5.test.js
@@ -1,30 +1,29 @@
-const rewire = require('rewire')
-const functions = rewire('./exercise5').__get__('functions')
-const isANumber = rewire('./exercise5').__get__('isANumber')
+const functions = require('./exercise5')
+const isANumber = require('./exercise5')
 
 
 
 
 test('It should give me the type of what was typed', () => {
-    expect(functions.type('teste')).toEqual('string')
+    expect(functions.a.type('teste')).toEqual('string')
 })
 
 test('It should give me the type of what was typed', () => {
-    expect(functions.type(22)).toEqual('number')
+    expect(functions.a.type(22)).toEqual('number')
 })
 
 
 test("It should give true if waht is typed has at least a space or false if it doesn't", () => {
-    expect(functions.whiteSpaces('Jon is cool')).toEqual('It has spaces');
+    expect(functions.a.whiteSpaces('Jon is cool')).toEqual('It has spaces');
 })
 test("It should give true if what is typed has at least a space or false if it doesn't", () => {
-    expect(functions.whiteSpaces('AllTogetherNow')).toBe("It doesn't has spaces");
+    expect(functions.a.whiteSpaces('AllTogetherNow')).toBe("It doesn't has spaces");
 })
 
 test('it should analyse if the given expression is a number or not', () => {
-    expect(isANumber(2)).toBeTruthy()
+    expect(isANumber.b(2)).toBeTruthy()
 })
 
 test('it should analyse if the given expression is a number or not', () => {
-    expect(isANumber('jon')).toBeFalsy()
+    expect(isANumber.b('jon')).toBeFalsy()
 })
