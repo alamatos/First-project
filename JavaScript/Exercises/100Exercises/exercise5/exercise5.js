7e13e29cbb6ceba0808b21d14e68e20b9fd3c235
Alamatos <alamatos@gmail.com>
2019-07-02 14:27:08 +0100

    exercise 5 final commit

--- JavaScript/Exercises/100Exercises/exercise5/exercise5.js
+++ JavaScript/Exercises/100Exercises/exercise5/exercise5.js
@@ -1,39 +1,83 @@
+const word = 'Just a test 1'
 const wordAnalysis = {
     type: (word) => typeof (word),
     whiteSpaces: (word) => {
         let wordAnalysis = word.includes(' ')
 
         if (wordAnalysis) {
-            return 'It has spaces'
+            return 'This expression has spaces.'
         } else {
-            return "It doesn't has spaces"
+            return "This expression doesn't has spaces."
         }
     },
     numberAnalysis: (word) => {
         let isANumber = typeof (word) === 'number' ? true : false
-        return isANumber
+        if (isANumber) {
+            return 'This expression is a number.'
+        } else {
+            return "This expression isn't a number."
+        }
+
     },
     alphabetic: (word) => {
         let patt = /[0-9]/g
         let patt1 = /\s/g
         let Alphabetic = patt.test(word) || patt1.test(word) === true ? false : true
 
-        return Alphabetic
+        if (Alphabetic) {
+            return 'This expression is alphabetic.'
+        } else {
+            return "This expression isn't alphabetic."
+        }
     },
     alphanumeric: (word) => {
         let patt = /[0-9]/g
         let patt1 = /\s/g
         let patt2 = /[a-z]/g
         let alphanumeric = patt.test(word) && patt1.test(word) && patt2.test(word)
-        return alphanumeric
+        if (alphanumeric) {
+            return 'This expression is alphanumeric.'
+        } else {
+            return "This expression isn't alphanumeric."
+        }
     },
-    uppercase : (word) => {
-        // let word2 = word.toUpperCase()
+    uppercase: (word) => {
         let analysisUpperCase = word == word.toUpperCase() ? true : false
-        return analysisUpperCase
+        if (analysisUpperCase) {
+            return 'This expression is in uppercase.'
+        } else {
+            return "This expression isn't in uppercase."
+        }
+    },
+    lowercase: (word) => {
+        let analysisLowercase = word == word.toLowerCase() ? true : false
+        if (analysisLowercase) {
+            return 'This expression is in lowercase.'
+        } else {
+            return "This expression isn't in lowercase."
+        }
+    },
+    capitalized: (word) => {
+        let wordUppercase = word.toUpperCase()
+        let wordCapitalized = word.replace(/^\w/, c => c.toUpperCase())
+
+        let analysis = word == wordCapitalized && wordUppercase !== wordCapitalized
+        if (analysis) {
+            return 'This expression is capitalized.'
+        } else {
+            return "This expression isn't capitalized."
+        }
     }
+
 }
-console.log(wordAnalysis.uppercase('AASSS 1'))
+console.log(wordAnalysis.type(word))
+console.log(wordAnalysis.whiteSpaces(word))
+console.log(wordAnalysis.numberAnalysis(word))
+console.log(wordAnalysis.alphabetic(word))
+console.log(wordAnalysis.alphanumeric(word))
+console.log(wordAnalysis.uppercase(word))
+console.log(wordAnalysis.lowercase(word))
+console.log(wordAnalysis.capitalized(word))
 
 
 function numberAnalysis(word) {
--- JavaScript/Exercises/100Exercises/exercise5/exercise5.test.js
+++ JavaScript/Exercises/100Exercises/exercise5/exercise5.test.js
@@ -12,18 +12,18 @@ test('It should give me the type of what was typed', () => {
 
 
 test("It should give true if waht is typed has at least a space or false if it doesn't", () => {
-    expect(wordAnalysis.firstPlace.whiteSpaces('Jon is cool')).toEqual('It has spaces');
+    expect(wordAnalysis.firstPlace.whiteSpaces('Jon is cool')).toBe('This expression has spaces.');
 })
 test("It should give true if what is typed has at least a space or false if it doesn't", () => {
-    expect(wordAnalysis.firstPlace.whiteSpaces('AllTogetherNow')).toBe("It doesn't has spaces");
+    expect(wordAnalysis.firstPlace.whiteSpaces('AllTogetherNow')).toBe("This expression doesn't has spaces.");
 })
 
 test('it should analyse if the given expression is a number or not - Object', () => {
-    expect(wordAnalysis.firstPlace.numberAnalysis(2)).toBeTruthy();
+    expect(wordAnalysis.firstPlace.numberAnalysis(2)).toBe('This expression is a number.');
 })
 
 test('it should analyse if the given expression is a number or not - Object', () => {
-    expect(wordAnalysis.firstPlace.numberAnalysis('jon')).toBeFalsy();
+    expect(wordAnalysis.firstPlace.numberAnalysis('jon')).toBe("This expression isn't a number.");
 })
 test('it should analyse if the given expression is a number or not - function', () => {
     expect(numberAnalysis.secondPlace(2)).toBeTruthy();
@@ -33,26 +33,44 @@ test('it should analyse if the given expression is a number or not - function', 
     expect(numberAnalysis.secondPlace('jon')).toBeFalsy();
 })
 test('it should analyze if the given expression is alphabetic or not - numbers', () => {
-    expect(wordAnalysis.firstPlace.alphabetic(123)).toBeFalsy();
+    expect(wordAnalysis.firstPlace.alphabetic(123)).toBe("This expression isn't alphabetic.");
 })
 test('it should analyze if the given expression is alphabetic or not - whitespaces', () => {
-    expect(wordAnalysis.firstPlace.alphabetic(' ')).toBeFalsy();
+    expect(wordAnalysis.firstPlace.alphabetic(' ')).toBe("This expression isn't alphabetic.");
 })
 test('it should analyze if the given expression is alphabetic or not - letters', () => {
-    expect(wordAnalysis.firstPlace.alphabetic('test')).toBeTruthy();
+    expect(wordAnalysis.firstPlace.alphabetic('test')).toBe('This expression is alphabetic.');
 })
 test('it should analyze if the given expression is alphanumeric or not', () => {
-    expect(wordAnalysis.firstPlace.alphanumeric('test')).toBeFalsy();
+    expect(wordAnalysis.firstPlace.alphanumeric('test')).toBe("This expression isn't alphanumeric.");
 })
 test('it should analyze if the given expression is alphanumeric or not', () => {
-    expect(wordAnalysis.firstPlace.alphanumeric('testin app')).toBeFalsy();
+    expect(wordAnalysis.firstPlace.alphanumeric('testin app')).toBe("This expression isn't alphanumeric.");
 })
 test('it should analyze if the given expression is alphanumeric or not', () => {
-    expect(wordAnalysis.firstPlace.alphanumeric('testing app 1')).toBeTruthy();
+    expect(wordAnalysis.firstPlace.alphanumeric('testing app 1')).toBe('This expression is alphanumeric.');
 })
 test('it should analyze if the given expression is all in uppercase or not', () => {
-    expect(wordAnalysis.firstPlace.alphanumeric('Testing app')).toBeFalsy();
+    expect(wordAnalysis.firstPlace.uppercase('Testing app')).toBe("This expression isn't in uppercase.");
 })
 test('it should analyze if the given expression is all in uppercase or not', () => {
-    expect(wordAnalysis.firstPlace.alphanumeric('TESTING APP')).toBeFalsy();
+    expect(wordAnalysis.firstPlace.uppercase('TESTING APP')).toBe('This expression is in uppercase.');
+})
+test('it should analyze if the given expression is all in lowercase or not', () => {
+    expect(wordAnalysis.firstPlace.lowercase('TESTING APP')).toBe("This expression isn't in lowercase.");
+})
+test('it should analyze if the given expression is all in lowercase or not', () => {
+    expect(wordAnalysis.firstPlace.lowercase('Testing app')).toBe("This expression isn't in lowercase.");
+})
+test('it should analyze if the given expression is all in lowercase or not', () => {
+    expect(wordAnalysis.firstPlace.lowercase('testing app')).toBe('This expression is in lowercase.');
+})
+test('it should analyze if the first letter of a given expression is capitalized or not', () => {
+    expect(wordAnalysis.firstPlace.capitalized('Testing app')).toBe('This expression is capitalized.');
+})
+test('it should analyze if the first letter of a given expression is capitalized or not', () => {
+    expect(wordAnalysis.firstPlace.capitalized('testing App')).toBe("This expression isn't capitalized.");
+})
+test('it should analyze if the first letter of a given expression is capitalized or not', () => {
+    expect(wordAnalysis.firstPlace.capitalized('TESTING APP')).toBe("This expression isn't capitalized.");
 })
